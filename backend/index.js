b8f91f2773d100e56596d8ff97d0b71ee7e389db
abhishekkumarsahani <126187842+abhishekkumarsahani@users.noreply.github.com>
2024-03-11 20:35:03 +0545

    Updated Cart Page
    Cart Page is Updated using API

--- backend/index.js
+++ backend/index.js
@@ -215,20 +215,49 @@ const fetchUser = async (req, res, next) => {
   if (!token) {
     res.status(401).send({ errors: "Please authenticate using valid token" });
   } else {
-    try{
-      const data = jwt.verify(token, 'secret_ecom');
+    try {
+      const data = jwt.verify(token, "secret_ecom");
       req.user = data.user;
       next();
-    }catch(error){
-      res.status(401).send({errors:"please authenticate using valid token"})
+    } catch (error) {
+      res.status(401).send({ errors: "please authenticate using valid token" });
     }
   }
 };
 
 //CREATING ENDPOINT FOR ADDING PRODUCTS IN CARDDATA
-app.post("/addtocart",fetchUser, async (req, res) => {
-  console.log(req.body,req.user);
+app.post("/addtocart", fetchUser, async (req, res) => {
+  console.log("added", req.body.itemId);
+
+  let userData = await Users.findOne({ _id: req.user.id });
+  userData.cartData[req.body.itemId] += 1;
+  await Users.findOneAndUpdate(
+    { _id: req.user.id },
+    { cartData: userData.cartData }
+  );
+  res.send("Added");
 });
+
+//CREATING ENDPOINT TO REMOVE PRODUCT FROM CARTDATA
+app.post("/removefromcart", fetchUser, async (req, res) => {
+  console.log("removed", req.body.itemId);
+
+  let userData = await Users.findOne({ _id: req.user.id });
+  if (userData.cartData[req.body.itemId] > 0)
+    userData.cartData[req.body.itemId] -= 1;
+  await Users.findOneAndUpdate(
+    { _id: req.user.id },
+    { cartData: userData.cartData }
+  );
+  res.send("Removed");
+});
+
+//CREATING ENDPOINT TO GET CARTDATA
+app.post('/getcart',fetchUser,async (req,res)=>{
+  console.log("GetCart");
+  let userData = await Users.findOne({_id:req.user.id});
+  res.json(userData.cartData);
+})
 
 app.listen(port, (error) => {
   if (!error) {
--- front-end/src/Components/CartItems/CartItems.jsx
+++ front-end/src/Components/CartItems/CartItems.jsx
@@ -23,7 +23,7 @@ const CartItems = () => {
             <div className="cartitems-format cartitems-format-main">
                 <img src={e.image} className='carticon-product-icon' alt="" />
                 <p>{e.name}</p>
-                <p>{e.new_price}</p>
+                <p>${e.new_price}</p>
                 <button className='cartitems-quantity'>{cartItems[e.id]}</button>
                 <p>${e.new_price*cartItems[e.id]}</p>
                 <img className='cartitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
